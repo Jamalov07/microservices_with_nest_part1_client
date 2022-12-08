01fd2f5fece07a2c4d7a461688f691128735e652
Jamalov <Jamalovn07@gmail.com>
2022-12-08 12:15:16 +0500

    first commit and part 1 its a client server

---
+++ src/product/product.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            'amqps://aixwvimp:iG1JtO9_GtbwgQ7UFRyTj27_f7mfoj8O@whale.rmq.cloudamqp.com/aixwvimp',
+          ],
+          queue: 'main_products_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
+export class ProductModule {}
